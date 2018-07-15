67ec4f7ae8c893e607de158d62df423db7295475
Hope <hkim@oscars.org>
2018-07-14 17:56:42 -0700

    Refactored code for readability

--- bin/storeData.js
+++ bin/storeData.js
@@ -12,12 +12,12 @@ const apiKey        = "8m8bkcVYqxE5j0vQL2wk1bpiBGibgaqCrOvwZVyU";
 const mgurl         = "mongodb://imakatman:1nsanse%26@ds117111.mlab.com:17111/picturesfrommars";
 const pathToDataDir = "../src/redux/data";
 
-let photoGrabbingData = [];
+let fetchSpecificRoverData = [];
 
 /*
  Fetch rovers data from Nasa API and store the
  id, queryName, and maxSol for each rover inside
- photoGrabbingData to be used when grabbing and storing
+ fetchSpecificRoverData to be used when grabbing and storing
  photos. Then resolve the data that is returned so that the
  chained callback for getRovers() can use it.
  */
@@ -32,7 +32,7 @@ function getRovers() {
             queryName: j.name.toLowerCase(),
             maxSol: Number(j.max_sol)
           };
-          photoGrabbingData.push(obj)
+          fetchSpecificRoverData.push(obj)
         })
         res(json);
       })
@@ -62,12 +62,63 @@ function updateRovers(database, data) {
   });
 }
 
-function grabPhotos() {
+function grabPhotos(database, fsrd, res, rej) {
+  fetch(`${apiUrl}/${fsrd.queryName}/photos?sol=${fsrd.maxSol}&api_key=${apiKey}`)
+    .then(resp => resp.json())
+    .then(json => {
+      const photos     = json.photos,
+            roverId    = photos[0].rover.id,
+            roverName  = photos[0].rover.name,
+            earth_date = photos[0].earth_date;
 
+      const formattedData = {
+        id: roverId,
+        name: roverName,
+        [earth_date]: photos.map(p => {
+          /*
+           * Returned data for photos repeats redundant data
+           * about the rover so the script must be specific about
+           * what data to send to the db
+           */
+          return {
+            [p.id]: {
+              id: p.id,
+              sol: p.id,
+              camera: p.camera,
+              img_src: p.img_src,
+              earth_date: p.earth_date,
+              rover: {
+                id: p.rover.id,
+                name: p.rover.name
+              }
+            }
+          }
+        })
+      };
+
+      console.log(formattedData)
+
+      console.log("first line of try")
+      database.collection("dates").updateOne(
+        { id: roverId },
+        { $set: formattedData },
+        { upsert: true },
+        function (err, result) {
+          console.log("first line of callback")
+          if (err) throw err;
+          console.log("updated", earth_date, "for", roverName)
+          res(earth_date);
+        }
+      );
+    })
+    // .then(json => console.log(json))
+    .catch(err => {
+      console.error(err);
+      return rej(err);
+    });
 }
 
-getRovers().then(vals => {
-  console.log("getRovers has resolved", vals);
+getRovers().then(resp => {
   /*
    Make api call with sols not earth dates but store data by earth date.
    It will be easier to loop through each day by sol.
@@ -81,66 +132,13 @@ getRovers().then(vals => {
     // ** End
 
     let dbo          = db.db("picturesfrommars");
-    const roversData = vals;
 
-    updateRovers(dbo, roversData.rovers);
+    updateRovers(dbo, resp.rovers);
 
-    const promises = photoGrabbingData.map(r => {
-      console.log("starting the photo storing process with", r.queryName)
+    const promises = fetchSpecificRoverData.map(fsrd => {
+      console.log("starting the photo storing process with", fsrd.queryName)
       return new Promise((res, rej) => {
-        fetch(`${apiUrl}/${r.queryName}/photos?sol=${r.maxSol}&api_key=${apiKey}`)
-          .then(resp => resp.json())
-          .then(json => {
-            const photos     = json.photos,
-                  roverId    = photos[0].rover.id,
-                  roverName  = photos[0].rover.name,
-                  earth_date = photos[0].earth_date;
-
-            const formattedData = {
-              id: roverId,
-              name: roverName,
-              [earth_date]: photos.map(p => {
-                /*
-                * Returned data for photos repeats redundant data
-                * about the rover so the script must be specific about
-                * what data to send to the db
-                */
-                return {
-                  [p.id]: {
-                    id: p.id,
-                    sol: p.id,
-                    camera: p.camera,
-                    img_src: p.img_src,
-                    earth_date: p.earth_date,
-                    rover: {
-                      id: p.rover.id,
-                      name: p.rover.name
-                    }
-                  }
-                }
-              })
-            };
-
-            console.log(formattedData)
-
-            console.log("first line of try")
-            dbo.collection("dates").updateOne(
-              { id: roverId },
-              { $set: formattedData },
-              { upsert: true },
-              function(err, result){
-                console.log("first line of callback")
-                if (err) throw err;
-                console.log("updated", earth_date, "for", roverName)
-                res(earth_date);
-              }
-            );
-          })
-          // .then(json => console.log(json))
-          .catch(err => {
-            console.error(err);
-            return rej(err);
-          });
+        grabPhotos(dbo, fsrd, res, rej);
       });
     });
 
