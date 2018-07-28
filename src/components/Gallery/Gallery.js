50afe4c2a3535b0a2af6d6fca6318ec969d36761
Hope <hkim@oscars.org>
2018-07-28 15:26:09 -0700

    In the process of setting up the component. I think I need to add some basic styling for what I want it to look like though.

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Route, NavLink } from 'react-router-dom';
-import Masonry from 'react-masonry-component';
 import LazyLoad from 'react-lazy-load';
 import { cameraChosen } from "../../redux/actions/userChooses";
 
